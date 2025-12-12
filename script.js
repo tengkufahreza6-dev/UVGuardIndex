70c11b2ad1b260768258d739d5cad001996ef014
tengkufahreza6-dev <tengkufahreza6@gmail.com>
2025-12-12 10:14:24 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -234,7 +234,7 @@ console.log("=".repeat(60));
     if (diff >= 1.0) {
         this.showNotification(
             `Kenaikan UV besar: +${diff.toFixed(1)} (Bahaya!)`,
-            "Bahaya"
+            "Danger"
         );
         // suara lebih tajam
         this.playTone(1200, 250, 0.25);
